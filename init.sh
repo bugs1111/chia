20fe7decde227f491e8b808f9fb51a06e8d44311
Bugs <bugs1111@gmail.com>
2021-05-13 15:59:26 +0800

    *

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 sudo apt install tmux vim -y
+
--- run.sh
+++ run.sh
@@ -1,22 +1,13 @@
-tmux new-session -s chia -n moni -d
+
+
+tmux new-session -d
 tmux split -h
 tmux split -v
 tmux split -v
 
-tmux send-keys -t chia:moni.1 'sudo dstat -cdm'
-tmux send-keys -t chia:moni.2 'df -ht ext4' C-m
-tmux send-keys -t chia:moni.3 'cd ~/hpool'
+tmux send-keys -t 0:0.0 'echo hi' C-m
 
-tmux new-window -n plots
-
-tmux split -h
-tmux split -v
-tmux split -h
-tmux split -v
-tmux split -h
-tmux split -v
-tmux split -h
-tmux split -v
+tmux selectp -t 1 
 
 tmux a
 
