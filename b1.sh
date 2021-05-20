234e7157409a004f692abe539740d65ff5ea66f5
Bugs <bugs1111@gmail.com>
2021-05-20 20:08:19 +0800

    *

--- b1.sh
+++ b1.sh
@@ -1,5 +1,5 @@
 source global.sh
 
-tmux send-keys -t $SNAME:$WNAME.0 "./py.sh 1 1" C-m
-tmux send-keys -t $SNAME:$WNAME.1 "./py.sh 2 1" C-m
-tmux send-keys -t $SNAME:$WNAME.2 "./py.sh 3 1" C-m
+tmux send-keys -t $SNAME:$WNAME.0 "./pb.sh 1 1" C-m
+tmux send-keys -t $SNAME:$WNAME.1 "./pb.sh 2 1" C-m
+tmux send-keys -t $SNAME:$WNAME.2 "./pb.sh 3 1" C-m
--- b3.sh
+++ b3.sh
@@ -1,5 +1,5 @@
 source global.sh
 
-tmux send-keys -t $SNAME:$WNAME.6 "./py.sh 1 3" C-m
-tmux send-keys -t $SNAME:$WNAME.7 "./py.sh 2 3" C-m
-tmux send-keys -t $SNAME:$WNAME.8 "./py.sh 3 3" C-m
+tmux send-keys -t $SNAME:$WNAME.6 "./pb.sh 1 3" C-m
+tmux send-keys -t $SNAME:$WNAME.7 "./pb.sh 2 3" C-m
+tmux send-keys -t $SNAME:$WNAME.8 "./pb.sh 3 3" C-m
