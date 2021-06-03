aa378b1efea13421fbe70fbf43ebeb7485ba106a
Bugs <bugs1111@gmail.com>
2021-06-04 00:46:28 +0800

    *

--- p.sh
+++ p.sh
@@ -5,28 +5,13 @@ if [ $# -lt 3 ]; then
 	exit
 fi
 
-FK=
-PK=
 
-if [ $1 == 'b' ]; then
-  FK=$FK_B
-  PK=$PK_B
-elif [ $1 == 'y' ]; then
-  FK=$FK_Y
-  PK=$PK_Y
-elif [ $1 == 'z' ]; then
-  FK=$FK_Z
-  PK=$PK_Z
-elif [ $1 == 'x' ]; then
-  FK=$FK_X
-  PK=$PK_X
-else
-  echo "  请清楚正确的KEY配置名！"
-  exit
-fi
+FK=`eval echo '$'"FK_$1"`
+PK=`eval echo '$'"PK_$1"`
+
 
 if [ -z $FK ] || [ -z $PK ]; then
-  echo "  配置‘$1’，没有对应正确的FK和PK，请检查配置文件！"
+  echo "  配置‘$1’（请注意大小写），没有对应正确的FK和PK，请检查配置文件！"
   exit
 fi
 
@@ -40,4 +25,6 @@ echo "开始清空临时盘 '/mnt/t$2/q$3' ..."
 rm -rf /mnt/t$2/q$3
 
 echo "开始绘制 'd$2' ($3/3)."
-cd CHIAROOT && . ./activate && chia plots create -f FK -p PK -k 32 -b 3200 -r 4 -n 999 -t /mnt/t$2/q$3 -2 /mnt/d$2 -d /mnt/d$2
+cd CHIAROOT 
+. ./activate
+chia plots create -f FK -p PK -k 32 -b 3200 -r 4 -n 999 -t /mnt/t$2/q$3 -2 /mnt/d$2 -d /mnt/d$2 | grep --color=auto -i -E "^Starting phase|^Time for phase|^Total time|error" 
---
+++ test/g.sh
@@ -0,0 +1,5 @@
+cat ./test/a.log | grep --color=auto -i -E "^Starting phase|^Time for phase|^Total time|error" 
+
+# Starting phase 
+# Time for phase 
+# Total time 
