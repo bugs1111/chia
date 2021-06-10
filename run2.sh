4d3160b1bc3453d12fd96642dd2a0ae0b2b97725
Bugs <bugs1111@gmail.com>
2021-06-10 21:35:17 +0800

    *

--- run2.sh
+++ run2.sh
@@ -33,6 +33,6 @@ tmux send-keys -t $SNAME:$WNAME.2 "sshpass -p chiachia sudo dstat -cmd" C-m
 # tmux send-keys -t $SNAME:$WNAME.10 "./hpool-miner-chia" C-m
 tmux send-keys -t $SNAME:$WNAME.0 "bash log.sh" C-m
 # tmux send-keys -t $SNAME:$WNAME.1 "echo 'bash p[b,x,y,z].sh 来启动绘制程序。'" C-m
-tmux send-keys -t $SNAME:$WNAME.3 "echo 'bash p[b,x,y,z].sh 来启动绘制程序。'" C-m
+tmux send-keys -t $SNAME:$WNAME.3 "echo 'bash p2[b,x,y,z].sh 来启动绘制程序。'" C-m
 tmux selectp -t 3
 tmux a -t $SNAME
